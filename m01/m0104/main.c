f1b217e27e60f57a6485cd955e8abfba581689d7
Paulo Oliveira <paulonuno@LAPTOP-NUHDUB1J.localdomain>
2020-10-23 21:43:32 +0100

    Resolução do exercico m0101 da ficha m04

--- m01/m0104/main.c
+++ m01/m0104/main.c
@@ -3,5 +3,27 @@
 int main()
 {
   // TODO
+printf ("Intruduza o texto\n");
+
+int a=0, b=0, c=0;
+
+char ch;
+while(ch != EOF) {
+ch = getchar();
+
+if(ch != ' ' && ch != '\n') {++a;};
+
+if ((ch == ' ' && ch-1 != ' ') || (ch == '\n' && (ch != ' ' || ch-1 != '\n'))) {++b;};
+
+if (ch == '\n') {++c;};
+
+}
+
+if (ch == EOF) { 
+printf ("linhas = %d; palavras = %d; letras = %d\n", c, b, a);
+};
+
+ 
+
   return 0;
 }
