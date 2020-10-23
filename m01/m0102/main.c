09a32493e5305eafe9408a3c98b0aa28a3a03fdf
Paulo Oliveira <paulonuno@LAPTOP-NUHDUB1J.localdomain>
2020-10-23 18:04:43 +0100

    Resolução do exercico m0101 da ficha m02

--- m01/m0102/main.c
+++ m01/m0102/main.c
@@ -3,5 +3,12 @@
 int main()
 {
   // TODO
+
+printf("%lu -> sizeof char\n", sizeof(char));
+printf("%lu -> sizeof short\n", sizeof(short));
+printf("%lu -> sizeof int\n", sizeof(int));
+printf("%lu -> sizeof long\n", sizeof(long));
+
   return 0;
 }
+
