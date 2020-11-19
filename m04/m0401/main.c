2a3f9ad3aad6a479e7754e4ae16cf68cb213abb9
Paulo Oliveira <paulonuno@LAPTOP-NUHDUB1J.localdomain>
2020-11-19 20:54:39 +0000

    Resolução do exercicio m0401 da ficha m04

--- m04/m0401/main.c
+++ m04/m0401/main.c
@@ -1,47 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double mod(double numerador, double denominador)
+{
+	int resto;
+	resto = (int)numerador/denominador;
+	return numerador - resto*denominador;
+}
+
 
 void calcula (double x, char y, double z)
 {
-	switch(y) 
+	switch(y)
 	{
 		case '+':
-			printf("%.2lf\n", x+y);
+			printf("%.2lf\n", x+z);
 			break;
 		case '*':
-			printf("%.2lf\n", x*y);
+			printf("%.2lf\n", x*z);
 			break;
 		case '/': 
-			printf("%.2lf\n", x/y);
+			if (z=='0')
+			{
+				printf("\ndivisão por zero\n");
+			}
+			else
+			printf("%.2lf\n", x/z);
 			break;
 		case '%':
-			printf("%.2lf\n", mod(x,y));
+			printf("%.2lf\n", mod(x,z));
+			break;
 	}
 
 }
 
 
-
-
-
-
-int main (int argc, char *argv[])
+int main ()
 {
-	if(argc !=4)
-	{
-		printf("\nErro de sintaxe\n");
-	}
 
-	double n1;
+	double n1,n3;
 	char n2;
-	double n3;
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
-	n3 = atoi(argv[3]);
-
-	calcula(n1, n2, n3);
-
+	while(printf("? "),scanf("%lf %c %lf", &n1, &n2, &n3) != EOF)
+	{
+		calcula(n1, n2, n3);
+	}
 	return 0;
 }
