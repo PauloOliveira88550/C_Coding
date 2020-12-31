70ba2fcd807df0983465cb5152b4bb25c85dcb7b
Paulo Oliveira <paulitinho.oliveira@gmail.com>
2020-12-31 15:03:47 +0000

    Resolução do exercicio m0801 da ficha m08

---
+++ m08/m0801/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+	printf("         /|  \n");
+	printf("        /b|  \n");
+	printf("       /  |  \n");
+	printf("      /   |  \n");
+	printf("   C /    | A\n");
+	printf("    /     |  \n");
+	printf("   /      |  \n");
+	printf("  /	  |  \n");
+	printf(" /a     c_|  \n");
+	printf("/_______|_|  \n");
+
+
+return 0;
+
+}
