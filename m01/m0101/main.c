fd121acaa62dc593ad0cfae9d20a0fda917364cb
Paulo Oliveira <paulitinho.oliveira@gmail.com>
2021-01-22 12:03:18 +0000

    Exercico m0101 ficha m01

--- m01/m0101/main.c
+++ m01/m0101/main.c
@@ -2,27 +2,38 @@
 
 int main()
 {
-  
-for(int i=1; i<13;i++){
-      
-      printf("     %d",i);}
-printf("\n");
-printf("----------------------------------------------------------------------------\n");
+  int i,j;
 
-for(int j=1;j<13;j++){
- 
-        printf("%2d",j); printf("|");
- 
-         for(int k=1;k<13;k++){
-          
-          int mul;
-          mul = j*k;
-        printf(" %5d",mul);
-}
-printf("|\n");
-}
-printf("----------------------------------------------------------------------------\n");
-  
- 
+  for(int i=-1 ; i<14; i++){
+    
+    for(int j=-1; j<14; j++)
+	{    
+    	if(i==0 || i==13)
+    		printf("---");
+
+    	else if((j==0 || j==13) && i>0)
+        	printf(" |");
+
+    	else if(i==-1 && j==-1)
+        	printf("    ");
+
+    	else if(i==-1 && j<13 && j>0)
+        	printf("%4d",j);
+
+    	else if(j==-1)
+        	printf("%2d",i);
+
+    	else if(i>0)
+			printf("%4d",i*j);
+      
+      	if(i==0 &&j==1)
+        printf("---------");
+      
+      	if(i==13 &&j==1)
+        printf("---------");
+    
+    }
+    printf("\n");
+  }
   return 0;
 }
