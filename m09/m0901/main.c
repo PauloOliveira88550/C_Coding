6374c278641d245667c1cb101861b7b4fedcb49f
Paulo Oliveira <paulitinho.oliveira@gmail.com>
2021-01-23 11:49:29 +0000

    exercicio m0901 ficha m09

--- m09/m0901/main.c
+++ m09/m0901/main.c
@@ -1,55 +1,47 @@
-#include <stdio.h>
+#include<stdio.h>
 
 #define TRUE 1
+#define FALSE 0
 
 struct data
 {
-    int d;
-    int m;
-    int a;
+    int dia, mes, ano;
 };
 
-
-int ler_data (struct data *dt);
-int validar_data (struct data *dt);
-void imprimir_data (struct data *dt);
-
-
-int ler_data (struct data *dt)
+int ler_data(struct data *dt)
 {
-int conta=0;
-
-    conta = scanf ("%2d/%2d/%4d", &dt->d, &dt->m, &dt->a);
-
-return (conta == 3 ? 1 : 0);
+    char c1, c2;
+    scanf("%d%c%d%c%d", &dt->dia, &c1, &dt->mes, &c2, &dt->ano);
+    
+    if(c1 == '/' && c2 == '/')
+        return 1;
+    else
+        return 0; 
 }
 
-int validar_data (struct data *dt)
+int validar_data(struct data *dt)
 {
-int n=0;
-int m[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    if ( dt->d > 0 && dt->d <= m[dt->m-1] )
-        n = 1;
-
-    if (dt->m == 2 && dt->d == 29 && (dt->a % 400 == 0 || (dt->a % 4 == 0 && dt->a % 100 != 0)) )
-        n = 1;
-
-return n;
+    static const int daysPerMonth[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	if(dt->dia > 0 && dt->dia <= daysPerMonth[dt->mes])
+		return 1;
+	
+	if (dt->mes == 2 && dt->dia == 29 && (dt->ano % 400 == 0 || (dt->ano % 4 == 0 && dt->ano % 100 != 0 )))
+		return 1;
+	
+	return 0;
 }
 
-
-void imprimir_data (struct data *dt)
+void imprimir_data(struct data *dt)
 {
-    printf("%d/%d/%d\n", dt->d, dt->m, dt->a);
+    printf("%d/%d/%d\n", dt->dia, dt->mes, dt->ano);
 }
-
 
 int main(int argc, char *argv[])
 {
     struct data dt;
+    printf("? ");
 
-    printf ("? ");
     if (ler_data(&dt) == TRUE)
     {
         if (validar_data(&dt) == TRUE)
@@ -59,6 +51,6 @@ int main(int argc, char *argv[])
     }
     else
         printf("error: invalid input!\n");
-
-return 0;
+    
+    return 0;
 }
