fccf0b8b1db560d0cf64c14add11cbc08d092797
Paulo Oliveira <paulitinho.oliveira@gmail.com>
2021-01-23 11:50:43 +0000

    exercicio m0902 ficha m09

--- m09/m0902/main.c
+++ m09/m0902/main.c
@@ -1,60 +1,46 @@
-#include <stdio.h>
-#define TRUE 1
+#include<stdio.h>
 
-struct pontos
+struct ponto
 {
-    int x;
-    int y;
+    float x, y;
 };
 
-int ler (struct pontos *pt);
-int verificar (struct pontos p1, struct pontos p2, struct pontos p3);
-void imprimir (struct pontos *pontos);
-
-int ler(struct pontos *pt)
+int ler_ponto(struct ponto *pt)
 {
-    return scanf ("%d %d", &pt->x, &pt->y);
+    return scanf("%f %f", &pt->x, &pt->y) == 2 ? 1 : 0;
 }
 
-int verificar(struct pontos p1, struct pontos p2, struct pontos p3)
+
+void imprimir_pontos(struct ponto *pontos)
 {
-int n; 
-	n = p1.x*p2.y + p1.y*p3.x + p2.x*p3.y   -   (p2.y*p3.x + p1.x*p3.y + p1.y*p2.x);
-return n;
+    printf("P1 = (%.2f, %.2f), P2 = (%.2f, %.2f), P3 = (%.2f, %.2f)\n", pontos[0].x, pontos[0].y, pontos[1].x, pontos[1].y, pontos[2].x, pontos[2].y);
 }
 
-void imprimir (struct pontos *pontos)
+int verificar_triangulo(struct ponto p1, struct ponto p2, struct ponto p3)
 {
-int i;
-    for (i = 0; i < 3 ; i++, pontos++)
-	{
-	 	printf("P%d = (%d, %d)", i+1, pontos->x, pontos->y);
-	}
+    return (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
 }
-
 
 int main(int argc, char *argv[])
 {
-int conta=0, i, flag=0;
-struct pontos pt[3];
-struct pontos *pontos;
+    struct ponto p[3];
 
-	pontos = &pt[0];
-    for (i = 0; i < 3 ; i++)
+    for (int i = 0; i < 3; i++)
     {
-        printf("P%d? ", i+1);
-	  	if (ler(&pt[i]) != 2)
-		   flag=1;
-	}
+        printf("P%d? ", i + 1);
+        if(!ler_ponto(&p[i]))
+        {
+            printf("error: invalid point!\n");
+            return 0;
+        }
+    }    
 
-    imprimir(pontos);
-	if ( verificar(pt[0], pt[1], pt[2]) )
-	 	printf("\nvalid triangle!\n");
-	else
-	 	printf("\ninvalid triangle!\n");
+    imprimir_pontos(p);
 
-    if (flag == 1)
-        printf ("\nerror: invalid point!\n");
-
- return 0;
+    if(verificar_triangulo(p[0], p[1], p[2]))
+        printf("valid triangle!\n");
+    else
+        printf("invalid triangle!\n");
+    
+    return 0;
 }
